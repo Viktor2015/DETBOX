b8da5981f4df3a9afd1d8710ad51f1448616b545
Viktor <viktor.work.2015@gmail.com>
2015-04-03 17:04:17 +0300

    '05_04-03-2'

--- dtbx_privateroom/js/dtbx_privateroom.js
+++ dtbx_privateroom/js/dtbx_privateroom.js
@@ -1,10 +1,8 @@
 (function ($) {
   Drupal.behaviors.dtbx_privateroom = {
     attach : function(context, settings) {
-      $("a[href*='/drupal/dtbx_privateroom/nojs']", context).addClass('ctools-use-modal');  		
+      $("a[href*='dtbx_privateroom/nojs']", context).addClass('ctools-use-modal');  		
     }
   };
 })(jQuery);
-
-//       $("a[href*='/drupal/dtbx_privateroom/nojs']", context).addClass('ctools-use-modal');  
-//       $("a", context).addClass('ctools-use-modal');  	   
+	   
