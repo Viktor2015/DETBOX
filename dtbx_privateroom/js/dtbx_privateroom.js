eb0867f5d1e4a4d2f6e801872c57aa0105d28a3b
Viktor <viktor.work.2015@gmail.com>
2015-04-07 12:10:57 +0300

    '15_04_07-01'

--- dtbx_privateroom/js/dtbx_privateroom.js
+++ dtbx_privateroom/js/dtbx_privateroom.js
@@ -2,7 +2,30 @@
   Drupal.behaviors.dtbx_privateroom = {
     attach : function(context, settings) {
       $("a[href*='dtbx_privateroom/nojs']", context).addClass('ctools-use-modal');  		
+
+	  // Для наших кнопок с ajax сначала подтверждение.
+      $('.need-confirm-button').each(function() {
+        // Настройки каждого ajax для каждого элемента в массиве Drupal.ajax, 
+        // где ключи - id элементов формы с '#ajax'.
+        // Таким образом достаточно просто подсунуть подтверждение в нужный момент.
+        var ajax = Drupal.ajax[this.id];
+        // Если эта функция возвращает false, запрос не производится.
+        ajax.options.beforeSend = function(xmlhttprequest, options) {
+          // Пользователь самоотверженно выбрал "Да".
+          if (confirm("Вы уверены?")) {
+            ajax.ajaxing = true;
+            return ajax.beforeSend(xmlhttprequest, options);
+          }
+          // Умный пользователь. Решил, что лучше не надо.
+          else {
+            // Если не установить в false, получим нереагирующую кнопку
+            // после отказа пользователя от заказа.
+            ajax.ajaxing = false;
+            return false;
+          }
+        };
+      });
+
     }
   };
 })(jQuery);
-	   
