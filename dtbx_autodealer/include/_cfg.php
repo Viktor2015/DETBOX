f7180c01b1da404ec4f245d1e0dabafc30784c8a
Viktor <viktor.work.2015@gmail.com>
2015-05-28 08:34:46 +0300

    '15_05_28'

---
+++ dtbx_autodealer/include/_cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+
+define("DS","/");
+define("T",".");
+define("TIMEZONE","Asia/Yekaterinburg");
+define("DEBUG",TRUE);
+define('WWW_ROOT', realpath(dirname(__FILE__)).DS);
+
+define('API_HOST',"http://auto2d.com");
+define('API_VERSION',"api");
+
+define("LOGIN","detbox");
+define("PASSWD","Ps4zxtQf9L2f");
